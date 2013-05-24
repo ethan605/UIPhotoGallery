89932037f2cb8464b488ec874fe2408ef57d4b2d
Ethan N <thanhnx.605@gmail.com>
2013-05-24 10:17:31 +0700

    Init XCode project

---
+++ UIPhotoGallery/UIPhotoGallery/UIAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  UIAppDelegate.h
+//  UIPhotoGallery
+//
+//  Created by Ethan Nguyen on 5/24/13.
+//  Copyright (c) 2013 Ethan Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ UIPhotoGallery/UIPhotoGallery/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  UIPhotoGallery
+//
+//  Created by Ethan Nguyen on 5/24/13.
+//  Copyright (c) 2013 Ethan Nguyen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "UIAppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([UIAppDelegate class]));
+    }
+}
